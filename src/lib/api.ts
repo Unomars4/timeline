661432428df43b101594bc07afc16a582a746f5b
Une Nyide <une.nyide@gmail.com>
2025-04-15 15:14:32 +0200

    perf: cache data retrieved

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,6 +3,7 @@ import { Timeline } from "../types";
 export async function getApiData() {
   const res: Response = await fetch(
     "https://arthurfrost.qflo.co.za/php/getTimeline.php",
+    { cache: "force-cache" },
   );
   const timeline: Timeline = (await res.json())["Timeline"];
   return timeline;
