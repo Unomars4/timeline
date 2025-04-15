b7a306277ae532cece5b360b6088f993d7c0ca9a
Une Nyide <une.nyide@gmail.com>
2025-04-15 06:22:53 +0200

    feat: add media player

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,24 @@ function App() {
 
   return (
     <main className="overflow-x-hidden font-inter w-5xl min-h-screen mx-auto">
-      <div className="clickable-thumbnails flex">
+      <div className="flex gap-x-2">
+        <img
+          src={`https://arthurfrost.qflo.co.za/${timeline[0].Icon}`}
+          className="w-[300px] h-[300px] rounded-md"
+        />
+        <div>
+          <p>Title: {timeline[0].Title}</p>
+          <p>Episode: {timeline[0].Episode}</p>
+          <p>Description: {timeline[0].Description}</p>
+          <p>Upload Date: {timeline[0].CreateDate}</p>
+          <audio controls>
+            <source
+              src={`https://arthurfrost.qflo.co.za/${timeline[0].Audio}`}
+            />
+          </audio>
+        </div>
+      </div>
+      <div className="clickable-thumbnails my-4 flex">
         <img
           className="object-cover rounded-full border-black border border-solid w-[50px] h-[50px]"
           src={`https://arthurfrost.qflo.co.za/${timeline[0].Icon}`}
